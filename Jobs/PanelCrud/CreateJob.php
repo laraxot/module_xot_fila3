aae7e606c138d716d6f4ba6b06eba453806061b3
marco76tv <marco.sottana@gmail.com>
2021-02-23 10:51:14 +0100

    .

--- Jobs/PanelCrud/CreateJob.php
+++ Jobs/PanelCrud/CreateJob.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
@@ -18,24 +17,3 @@ class CreateJob extends XotBaseJob {
         return $this->panel;
     }
 }
-=======
-<?php
-
-declare(strict_types=1);
-
-namespace Modules\Xot\Jobs\PanelCrud;
-
-use Modules\Xot\Contracts\PanelContract;
-
-//----------- Requests ----------
-//------------ services ----------
-
-/**
- * Class CreateJob.
- */
-class CreateJob extends XotBaseJob {
-    public function handle(): PanelContract {
-        return $this->panel;
-    }
-}
->>>>>>> 3c97c308c85924a62f31c89c71edfe23450749f0
