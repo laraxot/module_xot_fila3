a65c91251d643561eab24248b52345fe4300fd7d
Malebestia <malebestia@yahoo.it>
2021-04-08 23:56:46 +0200

    .

--- Models/Panels/XotBasePanel.php
+++ Models/Panels/XotBasePanel.php
@@ -1188,7 +1188,10 @@ abstract class XotBasePanel implements PanelContract {
     /**
      * @return mixed|string|null
      */
-    public function guid() {
+    public function guid(?bool $is_admin = null) {
+        if (isset($is_admin) && $is_admin) {
+            return $this->row->getKey();
+        }
         $row = $this->row;
         $key = $row->getRouteKeyName();
         $msg = [
@@ -1622,11 +1625,13 @@ abstract class XotBasePanel implements PanelContract {
     /**
      * @return string
      */
-    public function id() {
+    public function id(?bool $is_admin = null) {
         $curr = $this;
         $data = collect([]);
         while (null != $curr) {
-            $data->prepend($curr->postType().'-'.$curr->guid());
+            //$data->prepend($curr->postType().'-'.$curr->guid($is_admin));
+            $data->prepend($curr->postType().'-'.$curr->row->getKey());
+
             $curr = $curr->getParent();
         }
 
