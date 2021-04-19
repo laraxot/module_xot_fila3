aa116abb9758a4c5546c83ad2cec05237f2e62c4
marco76tv <marco.sottana@gmail.com>
2021-04-19 11:05:58 +0200

    .

--- Services/TreeService.php
+++ Services/TreeService.php
@@ -35,6 +35,7 @@ class TreeService {
                         'id' => $panel->id(),
                         'title' => $panel->title(),
                         'acts' => $acts,
+                        'open' => false,
                     ];
                 }
             );
