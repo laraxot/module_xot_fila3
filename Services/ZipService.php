a317819269a31ff65b0b411f436fe0cc3eb3bae5
marco76tv <marco.sottana@gmail.com>
2021-05-04 12:31:59 +0200

    .

--- Services/ZipService.php
+++ Services/ZipService.php
@@ -53,10 +53,58 @@ class ZipService {
 
         $zip = new ZipArchive();
         $filename_zip = Storage::disk('cache')->path($filename_zip);
+        $filename_zip = str_replace(['/', '\\'], [DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR], $filename_zip);
+
         if (true !== $zip->open($filename_zip, ZipArchive::CREATE)) {
             dddx(['NO']);
         }
 
+        //dddx(get_class_methods($zip));
+        /*
+        0 => "open"
+    1 => "setPassword"
+    2 => "close"
+    3 => "count"
+    4 => "getStatusString"
+    5 => "addEmptyDir"
+    6 => "addFromString"
+    7 => "addFile"
+    8 => "addGlob"
+    9 => "addPattern"
+    10 => "renameIndex"
+    11 => "renameName"
+    12 => "setArchiveComment"
+    13 => "getArchiveComment"
+    14 => "setCommentIndex"
+    15 => "setCommentName"
+    16 => "getCommentIndex"
+    17 => "getCommentName"
+    18 => "deleteIndex"
+    19 => "deleteName"
+    20 => "statName"
+    21 => "statIndex"
+    22 => "locateName"
+    23 => "getNameIndex"
+    24 => "unchangeArchive"
+    25 => "unchangeAll"
+    26 => "unchangeIndex"
+    27 => "unchangeName"
+    28 => "extractTo"
+    29 => "getFromName"
+    30 => "getFromIndex"
+    31 => "getStream"
+    32 => "setExternalAttributesName"
+    33 => "setExternalAttributesIndex"
+    34 => "getExternalAttributesName"
+    35 => "getExternalAttributesIndex"
+    36 => "setCompressionName"
+    37 => "setCompressionIndex"
+    38 => "setEncryptionName"
+    39 => "setEncryptionIndex"
+    */
+        if (0 == $rows->count()) {
+            return '<h3>Non ci sono file da aggiungere</h3>';
+        }
         foreach ($rows as $row) {
             $panel = Panel::get($row);
             if (null == $panel) {
@@ -67,6 +115,8 @@ class ZipService {
             $filename = $panel->pdfFilename();
 
             $path = Storage::disk('cache')->path($filename);
+            $path = str_replace(['/', '\\'], [DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR], $path);
+
             if (! File::exists($path)) {
                 $pdf_parz['filename'] = $filename;
                 $pdf_content = $panel->pdf($pdf_parz);
@@ -76,6 +126,7 @@ class ZipService {
             $zip->addFile($path, $filename);
         }
         $zip->close();
+
         if ('download' == $out) {
             return response()->download($filename_zip);
         }
