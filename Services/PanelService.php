08f511936ea648ae1cbf7155e688452b99f5f9dd
dade1987 <davidecavallini1987@gmail.com>
2021-08-19 13:57:39 +0200

    aggiunta eccezione a panelservice get

--- Services/PanelService.php
+++ Services/PanelService.php
@@ -71,7 +71,13 @@ class PanelService {
      * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
      * @throws \ReflectionException
      */
-    public static function get(Model $model): PanelContract {
+    public static function get(?Model $model): PanelContract {
+
+        if(!isset($model))
+        {
+            throw new \Exception("Instance of Eloquent Model is missing in Xot/PanelService:get() ");
+        }
+
         $panel = self::setModel($model)->panel();
         $post_type = $panel->postType();
         $name = Str::plural($post_type);
