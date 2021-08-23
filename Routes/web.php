6a38f5eb3a7d01e68a5a2db31818c7740048648f
marco76tv <marco.sottana@gmail.com>
2021-08-23 22:13:29 +0200

    .

--- Routes/web.php
+++ Routes/web.php
@@ -98,15 +98,10 @@ Route::group(
     );
 //}
 
-
 //custom route finche' siamo legati ai modelli
 // lista e' index, mostrare un elemento e' show ..
 
 Route::get('{lang}/feed/{item}', 'RssFeedController@feed');
 //Route::get('/sitemap.xml', 'SiteMapController@sitemap');
 //Route::get('{lang}/feed/{item}', 'RssFeedController@show');
-<<<<<<< HEAD
 Route::get('/sitemap.xml', 'SiteMapController@index');
-=======
-Route::get('/sitemap.xml', 'SiteMapController@index');
->>>>>>> b401fac (.)
