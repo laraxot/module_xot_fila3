f976ef9aac917c93a43818a9ed274981c6c42fec
marco76tv <marco.sottana@gmail.com>
2021-08-26 17:11:59 +0200

    .

--- Http/Controllers/XotBaseContainerController.php
+++ Http/Controllers/XotBaseContainerController.php
@@ -77,7 +77,13 @@ abstract class XotBaseContainerController extends Controller {
         $controller = $this->getController();
         $data = $request->all();
 
-        $panel = app($controller)->$method($data, $panel);
+        //$panel = app($controller)->$method($data, $panel);
+        $panel = app($controller)->$method($request);
+
+        if (! method_exists($panel, 'out')) {
+            return $panel;
+        }
+
 
         return $panel->out(
             [
