464d082f9c787b0be79abaf8554c34f853605687
Davide Cavallini <davidecavallini1987@gmail.com>
2021-09-12 18:17:00 +0200

    .

--- Http/Controllers/Admin/ModuleController.php
+++ Http/Controllers/Admin/ModuleController.php
@@ -8,6 +8,9 @@ use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Modules\Theme\Services\ThemeService;
+use Modules\Xot\Relations\CustomRelation;
+use Modules\Xot\Services\TenantService;
+use Nwidart\Modules\Facades\Module;
 
 /*
 * gestisce i module
@@ -23,10 +26,42 @@ use Modules\Theme\Services\ThemeService;
 //*/
 //*
 class ModuleController extends Controller {
-    public function index(Request $request): Renderable {
-        $params = optional(\Route::current())->parameters();
+
+    /**
+     * @return mixed
+     */
+    public function index(Request $request) {
+    
+        $params = optional(\Route::current())->parameters();        
 
         $home_view = $params['module'].'::admin.index';
+
+        if ('' != $request->_act) {
+
+            $module=Module::find($params['module']);
+
+            $panel='\Modules\\'.$module->getName().'\Models\Panels\HomePanel';
+
+            $panel=app($panel);
+
+            $home = TenantService::model('home');
+
+            $rows = new CustomRelation(
+                $home->newQuery(),
+                $home,
+                function ($relation): void {
+                    $relation->getQuery();
+                },
+                null,
+                null
+            );
+
+            $panel->setName('home');
+            $panel->setRows($rows);
+
+            return $panel->callItemActionWithGate($request->_act);
+        }
+
 
         if (\View::exists($home_view)) {
             return view()->make($home_view);
