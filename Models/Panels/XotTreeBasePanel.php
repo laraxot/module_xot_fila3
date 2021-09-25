719634d33570f51583b68df421bfd7c6ae704b65
Davide Cavallini <davidecavallini1987@gmail.com>
2021-09-25 12:05:44 +0200

    up

---
+++ Models/Panels/XotTreeBasePanel.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Models\Panels;
+
+class XotTreeBasePanel extends XotBasePanel {
+    /**
+     * @return bool
+     */
+    public function treeSonsCount() {
+        $count = 0;
+        foreach ($this->treeSons() as $treeSon) {
+            $count += count($treeSon);
+        }
+
+        return $count > 0;
+    }
+
+    /**
+     * @return array
+     */
+    public function treeSons() {
+        return [];
+    }
+
+    /**
+     * @return array
+     */
+    public function treeSonsIndexOrder() {
+        return [];
+    }
+
+    /**
+     * @return string
+     */
+    public function treeLabel() {
+        return $this->row->title;
+    }
+}
