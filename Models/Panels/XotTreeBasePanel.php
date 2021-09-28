241b28b60d21063dabc7df0e14f58ddf47cf2a28
Malebestia <malebestia@yahoo.it>
2021-09-28 11:22:34 +0200

    .

--- Models/Panels/XotTreeBasePanel.php
+++ Models/Panels/XotTreeBasePanel.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Models\Panels;
 
-class XotTreeBasePanel extends XotBasePanel {
+class XotTreeBasePanel extends XotBasePanel implements PanelTreeContract {
     /**
      * @return bool
      */
