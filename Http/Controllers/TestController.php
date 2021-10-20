e4b2c5df0092770d14041b5ded00791c4b51fc12
marco76tv <marco.sottana@gmail.com>
2021-10-20 14:18:42 +0200

    .

--- Http/Controllers/TestController.php
+++ Http/Controllers/TestController.php
@@ -5,9 +5,18 @@ declare(strict_types=1);
 namespace Modules\Xot\Http\Controllers;
 
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Request;
+use Modules\Xot\Services\PanelService;
 
 class TestController extends Controller {
     public function myRoutes() {
         dddx(getRouteParameters());
+    }
+
+    public function index(Request $request) {
+        $params = getRouteParameters();
+        $panel = PanelService::getByParams($params);
+
+        return $panel->out();
     }
 }
