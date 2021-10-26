2f313fc1278648b97d5e42fd80a1699957fa19c2
marco76tv <marco.sottana@gmail.com>
2021-10-26 09:56:57 +0200

    .

--- Models/Panels/ProfilePanel.php
+++ Models/Panels/ProfilePanel.php
@@ -1,24 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Xot\Models\Panels;
 
 use Illuminate\Http\Request;
-//--- Services --
 
-use Modules\Xot\Models\Panels\XotBasePanel;
+//--- Services --
 
 class ProfilePanel extends XotBasePanel {
     /**
      * The model the resource corresponds to.
-     *
-     * @var string
      */
     public static string $model = 'Modules\Xot\Models\Profile';
 
     /**
      * The single value that should be used to represent the resource when being displayed.
-     *
-     * @var string
      */
     public static string $title = 'title';
 
@@ -27,29 +24,27 @@ class ProfilePanel extends XotBasePanel {
      *
      * @var array
      */
-    public static $search = array (
-);
+    public static $search = [
+    ];
 
     /**
      * The relationships that should be eager loaded on index queries.
      *
      * @var array
      */
-    public function with():array {
+    public function with(): array {
         return [];
     }
 
-    public function search() :array {
-
+    public function search(): array {
         return [];
     }
 
     /**
-     * on select the option id
+     * on select the option id.
      *
-     * quando aggiungi un campo select, è il numero della chiave 
+     * quando aggiungi un campo select, è il numero della chiave
      * che viene messo come valore su value="id"
-     *
      */
     public function optionId(object $row) {
         return $row->getKey();
@@ -58,7 +53,7 @@ class ProfilePanel extends XotBasePanel {
     /**
      * on select the option label.
      */
-    public function optionLabel(object $row):string {
+    public function optionLabel(object $row): string {
         return $row->area_define_name;
     }
 
@@ -76,47 +71,34 @@ class ProfilePanel extends XotBasePanel {
      *
      * @return RowsContract
      */
-    public static function indexQuery(array $data, $query)
-    {
+    public static function indexQuery(array $data, $query) {
         //return $query->where('auth_user_id', $request->user()->auth_user_id);
         return $query;
     }
 
-
-
     /**
      * Get the fields displayed by the resource.
-     *
-     * @return array
-        'col_bs_size' => 6,
-        'sortable' => 1,
-        'rules' => 'required',
-        'rules_messages' => ['it'=>['required'=>'Nome Obbligatorio']],
         'value'=>'..',
      */
     public function fields(): array {
-        return array (
-  0 => 
-  (object) array(
-     'type' => 'Id',
-     'name' => 'id',
-     'comment' => NULL,
-  ),
-  1 => 
-  (object) array(
-     'type' => 'Integer',
-     'name' => 'auth_user_id',
-     'comment' => NULL,
-  ),
-);
+        return [
+            0 => (object) [
+                'type' => 'Id',
+                'name' => 'id',
+                'comment' => null,
+            ],
+            1 => (object) [
+                'type' => 'Integer',
+                'name' => 'auth_user_id',
+                'comment' => null,
+            ],
+        ];
     }
 
     /**
      * Get the tabs available.
-     *
-     * @return array
      */
-    public function tabs():array {
+    public function tabs(): array {
         $tabs_name = [];
 
         return $tabs_name;
@@ -124,10 +106,8 @@ class ProfilePanel extends XotBasePanel {
 
     /**
      * Get the cards available for the request.
-     *
-     * @return array
      */
-    public function cards(Request $request):array {
+    public function cards(Request $request): array {
         return [];
     }
 
@@ -135,28 +115,34 @@ class ProfilePanel extends XotBasePanel {
      * Get the filters available for the resource.
      *
      * @param \Illuminate\Http\Request $request
-     *
-     * @return array
      */
-    public function filters(Request $request = null):array {
+    public function filters(Request $request = null): array {
         return [];
     }
 
     /**
      * Get the lenses available for the resource.
-     *
-     * @return array
      */
-    public function lenses(Request $request):array {
+    public function lenses(Request $request): array {
         return [];
     }
 
     /**
      * Get the actions available for the resource.
-     *
-     * @return array
      */
-    public function actions():array {
+    public function actions(): array {
         return [];
+    }
+
+    public function isSuperAdmin(): bool {
+        //232 Access to an undefined property Illuminate\Database\Eloquent\Model::$user.
+        //$user = $this->row->user;
+        $user = $this->row->getRelationValue('user');
+
+        if (is_object($user->perm) && $user->perm->perm_type >= 4) {  //superadmin
+            return true;
+        }
+
+        return false;
     }
 }
