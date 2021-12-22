b3de953af27248ab45e6f91be03a079c46b5eaa2
marco76tv <marco.sottana@gmail.com>
2021-12-22 18:15:45 +0100

    .

--- Models/Panels/Policies/_ModulePanelPolicy.php
+++ Models/Panels/Policies/_ModulePanelPolicy.php
@@ -1,11 +1,14 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Modules\Xot\Models\Panels\Policies;
 
-use Illuminate\Auth\Access\HandlesAuthorization;
-use Modules\LU\Models\User as User;
-use Modules\Xot\Models\Panels\Policies\_ModulePanelPolicy as Panel;
-
-use Modules\Xot\Models\Panels\Policies\XotBasePanelPolicy;
+use Modules\Xot\Contracts\PanelContract;
+use Modules\Xot\Contracts\UserContract;
 
 class _ModulePanelPolicy extends XotBasePanelPolicy {
+    public function chooseAdmTheme(UserContract $user, PanelContract $panel) {
+        return true;
+    }
 }
