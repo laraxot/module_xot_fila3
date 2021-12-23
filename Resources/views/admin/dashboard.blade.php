cc84f12d3fc0f02373676951e4a554a1c1c133b6
Malebestia <malebestia@yahoo.it>
2021-12-23 19:17:11 +0100

    .

--- Resources/views/admin/dashboard.blade.php
+++ Resources/views/admin/dashboard.blade.php
@@ -1,12 +1,16 @@
 @extends('adm_theme::layouts.app')
-@section('page_heading',' ')
+@section('page_heading', ' ')
 @section('content')
-@include('formx::includes.flash')
+    @include('formx::includes.flash')
 
+    @php
+    //se in areas ho ancora vecchie aree oppure aree che utilizzo in altre basi, c'è qualche problema
+    //dddx([Auth::user()->areas->pluck('area_define_name'), Module::all(), Module::getByStatus(1)]);
+    @endphp
 
-@foreach(Auth::user()->areas as $area)
-    <x-dashboard.widget :area=$area />
-@endforeach
+    @foreach (Auth::user()->areas as $area)
+        <x-dashboard.widget :area=$area />
+    @endforeach
 
 
 @endsection
