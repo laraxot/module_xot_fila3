1e72ec14cb3f0fdf28b44bba798401d15995b089
Malebestia <malebestia@yahoo.it>
2022-01-16 20:58:46 +0100

    .

---
+++ Services/GraphToPdfService.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Services;
+
+use Barryvdh\Snappy\Facades\SnappyPdf;
+
+/**
+ * Class HtmlService.
+ */
+class GraphToPdfService {
+    public static function test() {
+        $pdf = SnappyPdf::loadView('chartjs');
+        $pdf->setOption('enable-javascript', true);
+        $pdf->setOption('javascript-delay', 5000);
+        $pdf->setOption('enable-smart-shrinking', true);
+        $pdf->setOption('no-stop-slow-scripts', true);
+
+        return $pdf->download('chart.pdf');
+    }
+}
