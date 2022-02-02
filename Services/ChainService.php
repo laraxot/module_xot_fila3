368a02889ece9714dee6d88118e70e99dadcf655
marco xot <marco.sottana@gmail.com>
2022-02-02 11:07:41 +0100

    .

--- Services/ChainService.php
+++ Services/ChainService.php
@@ -38,7 +38,8 @@ function chain($primary_field, $parent_field, $sort_field, $rows, $root_id = 0, 
 class ChainService {
     public array $table;
 
-    public \Illuminate\Database\Eloquent\Collection $rows;
+    //public \Illuminate\Database\Eloquent\Collection $rows;
+    public \Illuminate\Support\Collection $rows;
 
     public array $chain_table;
 
