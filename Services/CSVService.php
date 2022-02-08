3bd4917c72807cdcfe6ba9a784714587168d374c
Davide Cavallini <davidecavallini1987@gmail.com>
2022-02-08 13:55:45 +0100

    .

--- Services/CSVService.php
+++ Services/CSVService.php
@@ -8,4 +8,14 @@ namespace Modules\Xot\Services;
  * Class CSVService.
  */
 class CSVService {
+    public static function toArray($filename): array {
+        $lines = file($filename, FILE_IGNORE_NEW_LINES);
+
+        
+        foreach ($lines as $key => $value) {
+            $csv[$key] = str_getcsv($value);
+        }
+
+        return $csv;
+    }
 }
