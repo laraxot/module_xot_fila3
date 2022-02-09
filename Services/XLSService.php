8e6516edb4d0d91c493f045d9f84e4a5029191b7
Davide Cavallini <davidecavallini1987@gmail.com>
2022-02-09 13:00:27 +0100

    .

---
+++ Services/XLSService.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Services;
+
+class XLSService {
+    public static function toArray($filename) {
+        require_once __DIR__.'/vendor/autoload.php';
+
+        $reader = \Maatwebsite\Excel\Facades\Excel::load($filename);                  //this will load file
+        $results = $reader->noHeading()->get()->toArray();
+
+        dddx($results);
+    }
+}
