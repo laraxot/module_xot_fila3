aad008ce09d1ccec0ae54a15162aa24eb2f97c9f
marco xot <marco.sottana@gmail.com>
2022-02-28 17:46:25 +0100

    .

---
+++ Models/Panels/Policies/FeedPanelPolicy.php
@@ -0,0 +1,11 @@
+<?php
+namespace Modules\Xot\Models\Panels\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Modules\LU\Models\User as User;
+use Modules\Xot\Models\Panels\Policies\FeedPanelPolicy as Panel;
+
+use Modules\Xot\Models\Panels\Policies\XotBasePanelPolicy;
+
+class FeedPanelPolicy extends XotBasePanelPolicy {
+}
---
+++ Models/Panels/Policies/JobBatchPanelPolicy.php
@@ -0,0 +1,11 @@
+<?php
+namespace Modules\Xot\Models\Panels\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Modules\LU\Models\User as User;
+use Modules\Xot\Models\Panels\Policies\JobBatchPanelPolicy as Panel;
+
+use Modules\Xot\Models\Panels\Policies\XotBasePanelPolicy;
+
+class JobBatchPanelPolicy extends XotBasePanelPolicy {
+}
