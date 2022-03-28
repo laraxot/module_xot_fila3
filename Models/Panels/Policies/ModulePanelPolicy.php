71517705ac7f28106578242f356a9eb0522a629c
marco76tv <marco.sottana@gmail.com>
2022-03-28 18:30:44 +0200

    .

---
+++ Models/Panels/Policies/ModulePanelPolicy.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Models\Panels\Policies;
+
+use Modules\Xot\Contracts\UserContract;
+
+/**
+ * --.
+ */
+class ModulePanelPolicy extends XotBasePanelPolicy {
+    /**
+     * ---.
+     */
+    public function downloadDbModule(UserContract $user, PanelContract $panel) {
+        return true;
+    }
+}
