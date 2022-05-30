874b11ea08cafba72dc1516ac7ddeca29e3f4944
marco76tv <marco.sottana@gmail.com>
2022-05-30 18:11:37 +0200

    up

---
+++ Services/Memoization.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @link https://medium.com/@parvej.code/how-to-create-simple-memoization-helper-in-php-1eb3cdbfde7c
+ */
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Services;
+
+use Closure;
+
+class Memoization {
+
+    private static ?self $_instance = null;
+
+    protected array $memoized = [];
+
+    /**
+     * Undocumented function
+     *
+     */
+    public function __construct() {
+
+    }
+
+
+    /**
+     * getInstance.
+     *
+     * this method will return instance of the class
+     */
+    public static function getInstance(): self {
+        if (! self::$_instance) {
+            self::$_instance = new self();
+        }
+
+        return self::$_instance;
+    }
+
+    /**
+     * Undocumented function.
+     */
+    public static function make(): self {
+        return static::getInstance();
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param string $key
+     * @param Closure $callback
+     * @return void
+     */
+    public function memoize(string $key,Closure $callback) {
+        if (!isset($this->memoized[$key])) {
+            return $this->memoized[$key] = $callback();
+        }
+        return $this->memoized[$key];
+    }
+}
