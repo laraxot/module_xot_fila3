46fd6bbad000f5fcf24a0bbc0f4e6c006970ae20
marco76tv <marco.sottana@gmail.com>
2022-06-21 13:35:09 +0200

    up

--- View/Composers/XotBaseComposer.php
+++ View/Composers/XotBaseComposer.php
@@ -4,15 +4,51 @@ declare(strict_types=1);
 
 namespace Modules\Xot\View\Composers;
 
+use Exception;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use Modules\Theme\Models\Menu;
 use Modules\Theme\Models\MenuItem;
+use Nwidart\Modules\Facades\Module;
 
 /**
  * --.
  */
 abstract class XotBaseComposer {
+    /**
+     * Undocumented variable.
+     */
+    public string $module_name = '';
+
+    /**
+     * Undocumented function.
+     */
+    public function setModule(string $module_name): self {
+        $this->module_name = $module_name;
+
+        return $this;
+    }
+
+    /**
+     * Undocumented function.
+     *
+     * @param array|string|int|float|null ...$args
+     *
+     * @return mixed
+     */
+    public function call(string $func, ...$args) {
+        $module = Module::find($this->module_name);
+
+        $view_composer_class = 'Modules\\'.$module->getName().'\\View\Composers\\'.$module->getName().'Composer';
+        if (! class_exists($view_composer_class)) {
+            throw new Exception('['.$view_composer_class.']['.__LINE__.']['.__FILE__.']');
+        }
+        $view_composer = app($view_composer_class);
+
+        return $view_composer->{$func}(...$args);
+        // dddx([$view_composer, class_exists($view_composer)]);
+    }
+
     /**
      * --.
      */
