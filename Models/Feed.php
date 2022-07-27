13b09f7e4208e2168cd6ed24fb7d24e55051357d
marco76tv <marco.sottana@gmail.com>
2022-07-27 09:40:04 +0200

    rebase

--- Models/Feed.php
+++ Models/Feed.php
@@ -11,7 +11,9 @@ namespace Modules\Xot\Models;
 /**
  * Modules\Xot\Models\Feed
  *
- * @property int $post_id
+ * @property int $id
+ * @property string|null $created_by
+ * @property string|null $updated_by
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\Xot\Models\Image[] $images
@@ -21,8 +23,10 @@ namespace Modules\Xot\Models;
  * @method static \Illuminate\Database\Eloquent\Builder|Feed newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Feed query()
  * @method static \Illuminate\Database\Eloquent\Builder|Feed whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Feed wherePostId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feed whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feed whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Feed whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feed whereUpdatedBy($value)
  * @mixin \Eloquent
  */
 class Feed extends BaseModel {
