fa046544b8991dbf1e73f62fd43f7033aa05a62d
marco <marco@DESKTOP-9NHETKU.localdomain>
2022-08-08 10:26:01 +0200

    up

--- Models/Profile.php
+++ Models/Profile.php
@@ -27,18 +27,6 @@ use Spatie\Tags\HasTags;
  * @property string|null $phone
  * @property string|null $address
  * @property int|null $user_id
- * @property string|null $github_username
- * @property string|null $twitter
- * @property int|null $video_credits
- * @property int|null $clip_credits
- * @property int|null $clip_max_time
- * @property int|null $max_clips
- * @property string|null $review_cost
- * @property int|null $review_unit_time
- * @property int|null $max_search_days
- * @property int|null $max_alerts
- * @property int|null $moderator_id
- * @property int $parent_id
  * @property string|null $guid
  * @property string|null $image_src
  * @property-read string|null $lang
@@ -51,7 +39,7 @@ use Spatie\Tags\HasTags;
  * @property-read \Modules\Lang\Models\Post|null $post
  * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\Lang\Models\Post[] $posts
  * @property-read int|null $posts_count
- * @property \Illuminate\Database\Eloquent\Collection|\Modules\Tag\Models\Tag[] $tags
+ * @property \Illuminate\Database\Eloquent\Collection|\Spatie\Tags\Tag[] $tags
  * @property-write mixed $url
  * @property-read int|null $tags_count
  * @property-read \Modules\LU\Models\User|null $user
@@ -62,31 +50,19 @@ use Spatie\Tags\HasTags;
  * @method static \Illuminate\Database\Eloquent\Builder|Profile query()
  * @method static \Illuminate\Database\Eloquent\Builder|Profile whereAddress($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile whereBio($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereClipCredits($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereClipMaxTime($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile whereCreatedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile whereDeletedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile whereEmail($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile whereFirstname($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereGithubUsername($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereGuid($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereMaxAlerts($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereMaxClips($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereMaxSearchDays($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereModeratorId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereParentId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile wherePhone($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile wherePostType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereReviewCost($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereReviewUnitTime($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile whereSurname($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile whereTwitter($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile whereUpdatedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile whereUserId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Profile whereVideoCredits($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile withAllTags(\ArrayAccess|\Spatie\Tags\Tag|array $tags, ?string $type = null)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile withAllTagsOfAnyType($tags)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile withAnyTags(\ArrayAccess|\Spatie\Tags\Tag|array $tags, ?string $type = null)
--- Resources/views/admin/dashboard.blade.php
+++ Resources/views/admin/dashboard.blade.php
@@ -1,13 +1,1 @@
-@include('adm_theme::admin.dashboard.' . config('xra.adm_home', '02'))
-{{-- @extends('adm_theme::layouts.app')
-@section('page_heading', ' ')
-@section('content')
-    @include('theme::includes.flash')
-    ///{{ Route::currentRouteName() }} //admin.containers.index
-    @if (!empty(Auth::user()->areasUsed))
-        @foreach (Auth::user()->areasUsed as $area)
-            <x-dashboard.widget :area=$area />
-        @endforeach
-    @endif
-
-@endsection --}}
+@include('adm_theme::admin.dashboard.' . config('xra.adm_home', '01'))
