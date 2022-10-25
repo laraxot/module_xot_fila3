648cbf98127fff0359fd28d8d059e94551b97183
marco76tv <marco.sottana@gmail.com>
2022-10-25 13:41:39 +0200

    up

--- Models/Conf.php
+++ Models/Conf.php
@@ -10,18 +10,16 @@ use Sushi\Sushi;
 /**
  * Modules\Xot\Models\Conf.
  *
- * @property int                                                                  $id
- * @property string|null                                                          $name
- * @property \Illuminate\Database\Eloquent\Collection|\Modules\Xot\Models\Image[] $images
- * @property int|null                                                             $images_count
- *
+ * @property int $id
+ * @property string|null $name
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\Xot\Models\Image[] $images
+ * @property-read int|null $images_count
  * @method static \Modules\Xot\Database\Factories\ConfFactory factory(...$parameters)
- * @method static \Illuminate\Database\Eloquent\Builder|Conf  newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Conf  newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Conf  query()
- * @method static \Illuminate\Database\Eloquent\Builder|Conf  whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Conf  whereName($value)
- *
+ * @method static \Illuminate\Database\Eloquent\Builder|Conf newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Conf newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Conf query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Conf whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Conf whereName($value)
  * @mixin \Eloquent
  */
 class Conf extends BaseModel {
