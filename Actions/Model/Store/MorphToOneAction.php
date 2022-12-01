04b3a56bb182ff48a88c87e04735ca2663045934
marco xot <marco.sottana@gmail.com>
2022-12-01 18:45:11 +0100

    .

---
+++ Actions/Model/Store/MorphToOneAction.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Actions\Model\Store;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\QueueableAction\QueueableAction;
+
+class MorphToOneAction {
+    use QueueableAction;
+
+    public function __construct() {
+    }
+
+    public function execute(Model $row, object $relation) {
+        // dddx(['row' => $row, 'relation' => $relation, 'relation_data' => $relation->data]);
+
+        $rows = $relation->rows;
+
+        if (is_array($relation->data)) {
+            if (! isset($relation->data['lang'])) {
+                $relation->data['lang'] = \App::getLocale();
+            }
+            $rows->create($relation->data);
+        } else {
+            $rows->sync($relation->data);
+        }
+
+        return;
+
+        dddx([
+            'message' => 'wip',
+            'row' => $row,
+            'relation' => $relation,
+            'relation_rows' => $relation->rows->exists(),
+            't' => $row->{$relation->name},
+        ]);
+
+        dddx('wip');
+    }
+}
