409f64f7d3e667f234b75ff58761b49431af4206
marco xot <marco.sottana@gmail.com>
2022-12-05 18:41:08 +0100

    .

--- Helpers/Helper.php
+++ Helpers/Helper.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Str;
-use Modules\Cms\Services\RouteService;
 use Modules\Tenant\Services\TenantService;
 use Modules\Xot\Services\ArrayService;
 use Modules\Xot\Services\FileService;
@@ -245,7 +244,26 @@ if (! function_exists('inAdmin')) {
      * ---.
      */
     function inAdmin(array $params = []): bool {
-        return RouteService::inAdmin($params);
+        if (isset($params['in_admin'])) {
+            return (bool) $params['in_admin'];
+        }
+        // dddx(ThemeService::__getStatic('in_admin'));
+        /* Cannot call method get() on mixed
+        if (null !== config()->get('in_admin')) {
+            return config()->get('in_admin');
+        }
+        */
+        if ('admin' === \Request::segment(1)) {
+            return true;
+        }
+        $segments = \Request::segments();
+        if (\count($segments) > 0 && 'livewire' === $segments[0]) {
+            if (true === session('in_admin')) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
 
--- docs/source/docs/model/action/store.md
+++ docs/source/docs/model/action/store.md
@@ -18,10 +18,21 @@ laravel/Modules/Xot/Actions/Model/StoreAction.php
 
 It takes three params:
 
-* the *model* calling the store action
-* the *data* array to store
-*  the *rules* needed to validate data
+* the **model** calling the store action
+* the **data** array to store
+*  the **rules** needed to validate data
 
 ```php
 public function execute(Model $row, array $data, array $rules): Model {
 ```
+
+* If there is a fillable field named *lang*;
+* If *lang* is not set in data;
+* Then data get *lang* from the website Locale configuration.
+
+```php
+if (! isset($data['lang']) && \in_array('lang', $row->getFillable(), true)) {
+    $data['lang'] = app()->getLocale();
+}
+```
+
