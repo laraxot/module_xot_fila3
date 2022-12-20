9423bef18257ac4780247b5faa99ef8b7105f693
marco76tv <marco.sottana@gmail.com>
2022-12-20 16:55:10 +0100

    up

---
+++ Models/Panels/Policies/HomePanelPolicy.php
@@ -0,0 +1,11 @@
+<?php
+namespace Modules\Xot\Models\Panels\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Modules\LU\Models\User as User;
+use Modules\Xot\Models\Panels\Policies\HomePanelPolicy as Post; 
+
+use Modules\Xot\Models\Policies\XotBasePolicy;
+
+class HomePanelPolicy extends XotBasePolicy {
+}
