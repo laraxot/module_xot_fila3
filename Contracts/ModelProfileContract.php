af209eed5020b317c042799f2fd2a35dc89f6d00
Davide Cavallini <davidecavallini1987@gmail.com>
2023-01-02 16:57:32 +0100

    up

--- Contracts/ModelProfileContract.php
+++ Contracts/ModelProfileContract.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
- * Modules\Xot\Contracts\ModelContract.
+ * Modules\Xot\Contracts\ModelProfileContract.
  *
  * @property int                             $id
  * @property int|null                        $user_id
