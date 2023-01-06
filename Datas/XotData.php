812d7bfb78fe37b6180072c23440c91a1c4a74fb
marco76tv <marco.sottana@gmail.com>
2023-01-06 12:09:24 +0100

    .

--- Datas/XotData.php
+++ Datas/XotData.php
@@ -10,6 +10,17 @@ use Spatie\LaravelData\Data;
  * Undocumented class.
  */
 class XotData extends Data {
-    public string $main_module;
+    public string $main_module; // => 'Blog'
     public string $param_name = 'noset';
+
+    public string $adm_home;//' => '01',
+    public string $adm_theme;//' => 'AdminLTE',
+    //public bool $enable_ads;//' => '1',
+
+
+    public string $primary_lang;//' => 'it',
+    // 'pub_theme' => 'DirectoryBs5',
+    public string $pub_theme;//' => 'BsItalia',
+    public string $search_action;//' => 'it/videos',
+    public bool $show_trans_key = false;
 }
