67526a594c012668cf483aea60f94d1fe512f6b6
marco76tv <marco.sottana@gmail.com>
2023-01-11 11:04:38 +0100

    up

--- Datas/XotData.php
+++ Datas/XotData.php
@@ -13,13 +13,13 @@ class XotData extends Data {
     public string $main_module; // => 'Blog'
     public string $param_name = 'noset';
 
-    public string $adm_home; // ' => '01',
+    public string $adm_home = '01';
     public string $adm_theme; // ' => 'AdminLTE',
     // public bool $enable_ads;//' => '1',
 
-    public string $primary_lang; // ' => 'it',
+    public string $primary_lang = 'it';
     // 'pub_theme' => 'DirectoryBs5',
     public string $pub_theme; // ' => 'BsItalia',
-    public string $search_action; // ' => 'it/videos',
+    public string $search_action = 'it/videos';
     public bool $show_trans_key = false;
 }
