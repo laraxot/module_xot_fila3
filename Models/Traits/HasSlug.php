0148d99e3ade7ee8c333f904893213baf2420ecf
marco xot <marco.sottana@gmail.com>
2023-01-20 09:50:10 +0100

    .

--- Models/Traits/HasSlug.php
+++ Models/Traits/HasSlug.php
@@ -1,7 +1,4 @@
 <?php
-/**
- * da laravelio.
- */
 
 declare(strict_types=1);
 
@@ -14,7 +11,6 @@ use Illuminate\Support\Str;
  */
 trait HasSlug {
     public function slug(): ?string {
-        // return $this->slug;
         return $this->guid;
     }
 
@@ -22,7 +18,9 @@ trait HasSlug {
      * Undocumented function.
      */
     public function setSlugAttribute(string $slug): void {
+       
         $this->attributes['slug'] = $this->generateUniqueSlug($slug);
+      
     }
 
     public static function findBySlug(string $slug): self {
@@ -32,7 +30,7 @@ trait HasSlug {
     private function generateUniqueSlug(string $value): string {
         $slug = $originalSlug = Str::slug($value) ?: Str::random(5);
         $counter = 0;
-
+        
         while ($this->slugExists($slug, $this->exists ? $this->getKey() : null)) {
             ++$counter;
             $slug = $originalSlug.'-'.$counter;
@@ -42,6 +40,7 @@ trait HasSlug {
     }
 
     private function slugExists(string $slug, int $ignoreId = null): bool {
+        
         $query = $this->where('slug', $slug);
 
         if ($ignoreId) {
