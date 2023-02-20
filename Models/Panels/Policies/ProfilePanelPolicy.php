5f67e1b0a94a1509b06c46c75352290d5a6ab06f
marco76tv <marco.sottana@gmail.com>
2023-02-20 09:14:14 +0000

    up

---
+++ Models/Panels/Policies/ProfilePanelPolicy.php
@@ -0,0 +1,11 @@
+<?php
+namespace Modules\Xot\Models\Panels\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Modules\LU\Models\User as User;
+use Modules\Xot\Models\Panels\Policies\ProfilePanelPolicy as Post; 
+
+use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
+
+class ProfilePanelPolicy extends XotBasePanelPolicy {
+}
