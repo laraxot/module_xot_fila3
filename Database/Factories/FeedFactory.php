c8bfc9227c2821b5c2353e0cb40ebf8b5111d089
marco76tv <marco.sottana@gmail.com>
2023-03-29 10:18:20 +0200

    up

---
+++ Database/Factories/FeedFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+use Modules\Xot\Models\Feed;
+
+class FeedFactory extends Factory {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Feed::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition() {
+
+
+        return [
+            'created_at' => $this->faker->dateTime,
+            'updated_at' => $this->faker->dateTime
+        ];
+    }
+}
--- Models/Feed.php
+++ Models/Feed.php
@@ -11,24 +11,16 @@ namespace Modules\Xot\Models;
 /**
  * Modules\Xot\Models\Feed.
  *
- * @property int                                                                  $id
- * @property string|null                                                          $created_by
- * @property string|null                                                          $updated_by
- * @property \Illuminate\Support\Carbon|null                                      $created_at
- * @property \Illuminate\Support\Carbon|null                                      $updated_at
- * @property \Illuminate\Database\Eloquent\Collection|\Modules\Xot\Models\Image[] $images
- * @property int|null                                                             $images_count
- *
- * @method static \Modules\Xot\Database\Factories\FeedFactory factory(...$parameters)
- * @method static \Illuminate\Database\Eloquent\Builder|Feed  newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Feed  newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Feed  query()
- * @method static \Illuminate\Database\Eloquent\Builder|Feed  whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Feed  whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Feed  whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Feed  whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Feed  whereUpdatedBy($value)
- *
+ * @property int $post_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Modules\Xot\Database\Factories\FeedFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Feed newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Feed newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Feed query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Feed whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feed wherePostId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Feed whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class Feed extends BaseModel {
