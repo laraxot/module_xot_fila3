865448f0fd89198b179c94b17772ecc8ca2a8081
marco76tv <marco76tv@users.noreply.github.com>
2023-04-18 09:07:12 +0000

    Fix styling

--- Actions/GetModelClassByModelTypeAction.php
+++ Actions/GetModelClassByModelTypeAction.php
@@ -8,13 +8,15 @@ namespace Modules\Xot\Actions;
 
 use Spatie\QueueableAction\QueueableAction;
 
-class GetModelClassByModelTypeAction {
+class GetModelClassByModelTypeAction
+{
     use QueueableAction;
 
     /**
      * Execute the action.
      */
-    public function execute(string $model_type): string {
+    public function execute(string $model_type): string
+    {
         $model_class = collect(config('morph_map'))->get($model_type);
 
         return $model_class;
