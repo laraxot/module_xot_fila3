00f9a1540d81f9945aeff9f5599f647072bf249d
marco76tv <marco.sottana@gmail.com>
2023-04-18 13:21:31 +0200

    up

---
+++ Actions/GetModelTypeByModelAction.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @see https://github.com/protonemedia/laravel-ffmpeg
+ */
+declare(strict_types=1);
+
+namespace Modules\Xot\Actions;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Spatie\QueueableAction\QueueableAction;
+
+class GetModelTypeByModelAction {
+    use QueueableAction;
+
+    /**
+     * Execute the action.
+     */
+    public function execute(Model $model): string {
+        $model_class = Str::snake(class_basename($model));
+
+        return $model_class;
+    }
+}
