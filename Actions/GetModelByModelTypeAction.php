36ac755bf766a65f7a7b836fd7a3a5217fcda098
marco76tv <marco76tv@users.noreply.github.com>
2023-04-18 11:23:23 +0000

    Fix styling

--- Actions/GetModelByModelTypeAction.php
+++ Actions/GetModelByModelTypeAction.php
@@ -9,13 +9,15 @@ namespace Modules\Xot\Actions;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\QueueableAction\QueueableAction;
 
-class GetModelByModelTypeAction {
+class GetModelByModelTypeAction
+{
     use QueueableAction;
 
     /**
      * Execute the action.
      */
-    public function execute(string $model_type, ?string $model_id): ?Model {
+    public function execute(string $model_type, ?string $model_id): ?Model
+    {
         $model_class = app(GetModelClassByModelTypeAction::class)->execute($model_type);
         $model = app($model_class);
         if (null != $model_id) {
--- Actions/GetModelTypeByModelAction.php
+++ Actions/GetModelTypeByModelAction.php
@@ -10,13 +10,15 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 use Spatie\QueueableAction\QueueableAction;
 
-class GetModelTypeByModelAction {
+class GetModelTypeByModelAction
+{
     use QueueableAction;
 
     /**
      * Execute the action.
      */
-    public function execute(Model $model): string {
+    public function execute(Model $model): string
+    {
         $model_class = Str::snake(class_basename($model));
 
         return $model_class;
