0f0555c2f387ee545d105d29891bb7946f3bb0ae
marco76tv <marco.sottana@gmail.com>
2023-04-18 13:09:25 +0200

    up

--- Models/Panels/ProfilePanel.php
+++ Models/Panels/ProfilePanel.php
@@ -9,8 +9,7 @@ use Illuminate\Http\Request;
 use Modules\Cms\Models\Panels\XotBasePanel;
 use Modules\Xot\Contracts\RowsContract;
 
-class ProfilePanel extends XotBasePanel
-{
+class ProfilePanel extends XotBasePanel {
     /**
      * The model the resource corresponds to.
      */
@@ -32,13 +31,11 @@ class ProfilePanel extends XotBasePanel
     /**
      * The relationships that should be eager loaded on index queries.
      */
-    public function with(): array
-    {
+    public function with(): array {
         return [];
     }
 
-    public function search(): array
-    {
+    public function search(): array {
         return [];
     }
 
@@ -48,7 +45,7 @@ class ProfilePanel extends XotBasePanel
     //  * quando aggiungi un campo select, è il numero della chiave
     //  * che viene messo come valore su value="id"
     //  *
-    //  * @param Modules\Xot\Models\Profile $row
+    //  * @param \Modules\Xot\Models\Profile $row
     //  *
     //  * @return int|string|null
     //  */
@@ -64,7 +61,7 @@ class ProfilePanel extends XotBasePanel
     // /**
     //  * on select the option label.
     //  *
-    //  * @param Modules\Xot\Models\Profile $row
+    //  * @param \Modules\Xot\Models\Profile $row
     //  */
     // public function optionLabel($row): string {
     //     return 'To Set';
@@ -73,8 +70,7 @@ class ProfilePanel extends XotBasePanel
     /**
      * index navigation.
      */
-    public function indexNav(): ?Renderable
-    {
+    public function indexNav(): ?Renderable {
         return null;
     }
 
@@ -85,8 +81,7 @@ class ProfilePanel extends XotBasePanel
      *
      * @return RowsContract
      */
-    public function indexQuery(array $data, $query)
-    {
+    public function indexQuery(array $data, $query) {
         // return $query->where('user_id', $request->user()->id);
         return $query;
     }
@@ -95,8 +90,7 @@ class ProfilePanel extends XotBasePanel
      * Get the fields displayed by the resource.
         'value'=>'..',
      */
-    public function fields(): array
-    {
+    public function fields(): array {
         return [
             0 => (object) [
                 'type' => 'Id',
@@ -114,8 +108,7 @@ class ProfilePanel extends XotBasePanel
     /**
      * Get the tabs available.
      */
-    public function tabs(): array
-    {
+    public function tabs(): array {
         $tabs_name = [];
 
         return $tabs_name;
@@ -124,8 +117,7 @@ class ProfilePanel extends XotBasePanel
     /**
      * Get the cards available for the request.
      */
-    public function cards(Request $request): array
-    {
+    public function cards(Request $request): array {
         return [];
     }
 
@@ -134,24 +126,21 @@ class ProfilePanel extends XotBasePanel
      *
      * @param \Illuminate\Http\Request $request
      */
-    public function filters(Request $request = null): array
-    {
+    public function filters(Request $request = null): array {
         return [];
     }
 
     /**
      * Get the lenses available for the resource.
      */
-    public function lenses(Request $request): array
-    {
+    public function lenses(Request $request): array {
         return [];
     }
 
     /**
      * Get the actions available for the resource.
      */
-    public function actions(): array
-    {
+    public function actions(): array {
         return [];
     }
 }
