a85e1f17bc796899437002b789b2a2af13f8443e
marco76tv <marco.sottana@gmail.com>
2023-04-18 11:05:12 +0200

    up

---
+++ Actions/GetModelClassByModelTypeAction.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @see https://github.com/protonemedia/laravel-ffmpeg
+ */
+declare(strict_types=1);
+
+namespace Modules\Xot\Actions;
+
+use Spatie\QueueableAction\QueueableAction;
+
+class GetModelClassByModelTypeAction {
+    use QueueableAction;
+
+    /**
+     * Execute the action.
+     */
+    public function execute(string $model_type): string {
+        $model_class = collect(config('morph_map'))->get($model_type);
+
+        return $model_class;
+    }
+}
