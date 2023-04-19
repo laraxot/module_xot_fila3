f56a795b9d2805ee399546ca1aca3227a713f9bd
marco76tv <marco.sottana@gmail.com>
2023-04-19 16:39:38 +0200

    up

--- Actions/GetModelByModelTypeAction.php
+++ Actions/GetModelByModelTypeAction.php
@@ -16,12 +16,15 @@ class GetModelByModelTypeAction
     /**
      * Execute the action.
      */
-    public function execute(string $model_type, ?string $model_id): ?Model
+    public function execute(string $model_type, ?string $model_id): Model
     {
         $model_class = app(GetModelClassByModelTypeAction::class)->execute($model_type);
         $model = app($model_class);
         if (null != $model_id) {
             $model = $model->find($model_id);
+        }
+        if (null == $model) {
+            throw new \Exception('['.__LINE__.']['.__FILE__.']');
         }
 
         return $model;
