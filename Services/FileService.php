ee9ece655f65066592b77c2d6b5b29830527393b
marco76tv <marco76tv@users.noreply.github.com>
2023-06-07 19:46:23 +0000

    Check & fix styling

--- Services/FileService.php
+++ Services/FileService.php
@@ -838,7 +838,7 @@ class FileService
     {
         $namespace = Str::replace('/', '\\', $namespace);
         $components_json = $path.'/_components.json';
-        $components_json =self::fixPath($components_json);
+        $components_json = self::fixPath($components_json);
         $path = self::fixPath($path);
         /*
         throw new Exception ??
@@ -848,7 +848,7 @@ class FileService
             }
         }
         */
-        
+
         $exists = File::exists($components_json);
         if ($exists && ! $force_recreate) {
             $content = File::get($components_json);
@@ -861,7 +861,7 @@ class FileService
 
             return $comps;
         }
-        //$files = File::allFiles(\dirname($components_json));
+        // $files = File::allFiles(\dirname($components_json));
         $files = File::allFiles($path);
 
         $comps = [];
@@ -907,7 +907,7 @@ class FileService
             $old_content = File::get($components_json);
         }
         if ($old_content !== $content) {
-          //  File::put($components_json, $content);
+            //  File::put($components_json, $content);
         }
 
         return $comps;
