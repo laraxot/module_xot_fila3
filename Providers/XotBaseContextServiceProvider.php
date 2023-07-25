75e36a9bde9426f0d8912c155b42f363a1eb14f0
marco76tv <marco76tv@users.noreply.github.com>
2023-07-25 12:31:39 +0000

    Check & fix styling

--- Providers/XotBaseContextServiceProvider.php
+++ Providers/XotBaseContextServiceProvider.php
@@ -7,25 +7,29 @@ namespace Modules\Xot\Providers;
 use Savannabits\FilamentModules\ContextServiceProvider;
 use Savannabits\FilamentModules\FilamentModules;
 
-class XotBaseContextServiceProvider extends ContextServiceProvider {
+class XotBaseContextServiceProvider extends ContextServiceProvider
+{
     public static string $name = 'xot-filament';
     public static string $module = 'Xot';
 
-    public function packageRegistered(): void {
+    public function packageRegistered(): void
+    {
         $this->app->booting(function () {
             $this->registerConfigs();
         });
         parent::packageRegistered();
     }
 
-    public function registerConfigs() {
+    public function registerConfigs()
+    {
         $this->mergeConfigFrom(
             app('modules')->findOrFail(static::$module)->getExtraPath('Config/'.static::$name.'.php'),
             static::$name
         );
     }
 
-    public function boot() {
+    public function boot()
+    {
         parent::boot();
         app(FilamentModules::class)->prepareDefaultNavigation(static::$module, static::$name);
     }
