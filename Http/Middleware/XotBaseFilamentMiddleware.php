a08d6d56c344aa5eed914183c4aaaece43c46c28
marco76tv <marco.sottana@gmail.com>
2023-07-27 11:00:27 +0200

    up

--- Http/Middleware/XotBaseFilamentMiddleware.php
+++ Http/Middleware/XotBaseFilamentMiddleware.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Http\Middleware;
 
+use Illuminate\Support\Str;
+use Webmozart\Assert\Assert;
 use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use Illuminate\Support\Str;
 
 abstract class XotBaseFilamentMiddleware extends Middleware
 {
@@ -37,7 +38,7 @@ abstract class XotBaseFilamentMiddleware extends Middleware
     protected function authenticate($request, array $guards): void
     {
         $context = $this->getContextName();
-        $guardName = config("$context.auth.guard");
+        Assert::string($guardName = config("$context.auth.guard"),'wip');
         $guard = $this->auth->guard($guardName);
 
         if (! $guard->check()) {
