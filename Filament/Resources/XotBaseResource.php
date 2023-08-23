78047cfb0db75a1232e5393a188a7d8ebf3e3c78
marco76tv <marco.sottana@gmail.com>
2023-08-23 16:22:54 +0200

    up

---
+++ Filament/Resources/XotBaseResource.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Filament\Resources;
+
+use Filament\Resources\Resource;
+use Illuminate\Support\Str;
+use Savannabits\FilamentModules\Concerns\ContextualResource;
+
+abstract class XotBaseResource extends Resource
+{
+    use ContextualResource;
+
+    protected static ?string $model = null;
+    // protected static ?string $navigationIcon = 'heroicon-o-bell';
+
+    // protected static ?string $navigationLabel = 'Custom Navigation Label';
+    // protected static ?string $activeNavigationIcon = 'heroicon-s-document-text';
+    // protected static bool $shouldRegisterNavigation = false;
+    // protected static ?string $navigationGroup = 'Parametri di Sistema';
+    protected static ?int $navigationSort = 3;
+
+    public static function trans(string $key): string
+    {
+        $moduleNameLow = Str::lower(static::getModuleName());
+        $modelNameSlug = Str::kebab(class_basename(static::$model));
+        $res = $moduleNameLow.'::'.$modelNameSlug.'.'.$key;
+        $trans = __($res);
+
+        return $trans;
+    }
+
+    public static function getModel(): string
+    {
+        // if (null != static::$model) {
+        //    return static::$model;
+        // }
+        $moduleName = static::getModuleName()->toString();
+        $modelName = Str::before(class_basename(get_called_class()), 'Resource');
+        $res = 'Modules\\'.$moduleName.'\Models\\'.$modelName;
+        static::$model = $res;
+
+        return $res;
+    }
+
+    public static function getPluralModelLabel(): string
+    {
+        return static::trans('navigation.plural');
+    }
+
+    public static function getNavigationLabel(): string
+    {
+        return static::trans('navigation.name');
+        // return static::trans('navigation.plural');
+    }
+
+    public static function getNavigationGroup(): string
+    {
+        return static::trans('navigation.group.name');
+    }
+}
