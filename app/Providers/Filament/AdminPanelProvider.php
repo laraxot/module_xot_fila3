ed8a403118508da70163d30c880db6fd9e585dc2
marco76tv <marco.sottana@gmail.com>
2025-02-26 16:46:05 +0100

    up

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Providers\Filament;
 
+/**
+ * Undocumented class
+ */
 class AdminPanelProvider extends XotBasePanelProvider
 {
     protected string $module = 'Xot';
