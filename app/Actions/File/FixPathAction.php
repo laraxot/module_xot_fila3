f661968d3c250a72aa94fdbb055bb0db9574a146
marco76tv <marco.sottana@gmail.com>
2025-02-26 15:36:17 +0100

    up

--- app/Actions/File/FixPathAction.php
+++ app/Actions/File/FixPathAction.php
@@ -1,15 +1,13 @@
 <?php
 
-/**
- * moved from fileservice.
- */
-
 declare(strict_types=1);
 
 namespace Modules\Xot\Actions\File;
 
 use Spatie\QueueableAction\QueueableAction;
-
+/**
+ * moved from fileservice.
+ */
 class FixPathAction
 {
     use QueueableAction;
