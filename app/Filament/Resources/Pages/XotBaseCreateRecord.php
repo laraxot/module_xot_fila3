aef810c39dacf41efef386c5a712c0af0215598b
marco76tv <marco.sottana@gmail.com>
2025-02-26 16:51:34 +0100

    Auto update submodule

--- app/Filament/Resources/Pages/XotBaseCreateRecord.php
+++ app/Filament/Resources/Pages/XotBaseCreateRecord.php
@@ -6,6 +6,9 @@ namespace Modules\Xot\Filament\Resources\Pages;
 
 use Filament\Resources\Pages\CreateRecord as FilamentCreateRecord;
 
+/**
+ * Undocumented class
+ */
 abstract class XotBaseCreateRecord extends FilamentCreateRecord
 {
     // ...
