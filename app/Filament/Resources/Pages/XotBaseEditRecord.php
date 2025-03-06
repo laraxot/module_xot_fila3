30a64e09424bb88c76b0edb0859c9fcc1cd30fd9
marco76tv <marco.sottana@gmail.com>
2025-03-06 11:08:28 +0100

    .

--- app/Filament/Resources/Pages/XotBaseEditRecord.php
+++ app/Filament/Resources/Pages/XotBaseEditRecord.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Modules\Xot\Filament\Resources\Pages;
 
 use Filament\Forms\Form;
+use Filament\Resources\Pages\EditRecord as FilamentEditRecord;
 
 /**
  * Undocumented class.
